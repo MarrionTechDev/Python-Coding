716291ce6280792d951958e2676380034d683f23
Marrion-Dev <oduromarrion@gmail.com>
2026-03-03 20:22:51 -0500

    Refactor CLI into functions and add input validation (v1.1)
    Refactor task management into functions for better structure and readability.
    Refactor daily to-do list app with functions

--- Daily-To-Do-List-App/daily-to-do-list.py
+++ Daily-To-Do-List-App/daily-to-do-list.py
@@ -1,42 +1,76 @@
-Tasks = ["Wake up", "Clean", "Study", "Draw", "Code"]
-while True:
-    print("\n---Menu--- \n1. Add task\n2. View tasks\n3. Remove task\n4. Exit \n")
-    user_input = input("Enter number for option: ")
+tasks = ["Wake Up", "Stretch", "Study", "Draw"]
 
-    task = ""
-    if user_input == "1":
-        while task != "n":
-            task = input("\nAdd a task (n to return): ")
-            Tasks.append(task)
-        Tasks.pop()
+def add_task(task):
+    while True:
+        task = input("\nAdd a task (n to return): ")
+        if task == "":
+            print("Invalid input")
+            continue
+        if task == "n":
+            break
+        tasks.append(task)
 
-    elif user_input == "2":
-        print("\n--Tasks--")
-        if Tasks == []:
-            print("No task entered")
-        for i in Tasks:
-            print(f"{i+1}. {task}")
-        back = input("\nn to return: ")
-        continue
+def view_task():
+    print("\n--Tasks--")
+    if not tasks:
+        print("No task entered")
 
-    elif user_input == "3":
-        print("\n--Tasks--")
-        if Tasks == []:
-            print("No task entered")
-        for i in Tasks:
-            print(f"{i+1}. {task}")
-        remove = int(input("\nSelect task number you want to delete: "))
-        for i in range(len(Tasks)):
-            if remove-1 == i:
-                Tasks.pop(i)
-        print(f"\nTask{remove} deleted")
+    for i, task in enumerate(tasks, start = 1) :
+        print(f"{i}) {task}")
 
-        back = input("\nn to return: ")
-        continue
+def remove_task():
+    while True:
+            remove = input("\nSelect task number you want to delete (n to return): ")
 
-    elif user_input == "4":
-        print("Exit")
-        break
+            if remove == "n":
+                break  
+
+            if not remove.isdigit():
+                print("Invalid Input")
+                continue
+            
+            remove = int(remove)
+
+            if not 1 <= remove <= len(tasks):
+                print("Task number is out of range")
+                continue
+        
+            tasks.pop(remove - 1 )
+            print(f"\nTask {remove} was deleted")
+            break
     
-    else:
-        print("Invalid Entry")
+def go_back():
+    back = input("\nn to return: ").lower()
+    while back != "n":
+        print("Invalid input")
+        back = input("\nn to return: ").lower()
+    
+def run():
+    while True:
+        print("\n---Menu--- \n1. Add task\n2. View tasks\n3. Remove task\n4. Exit \n")
+        user_input = input("Enter number for option: ")
+
+        if user_input == "1":
+
+            add_task(tasks)
+
+        elif user_input == "2":
+            view_task()
+
+            go_back()
+
+        elif user_input == "3":
+            view_task()
+
+            remove_task()
+
+            go_back()
+
+        elif user_input == "4":
+            print("Exit")
+            break
+        
+        else:
+            print("Invalid Entry")
+
+run()
